0a2c26b1e3121e6e90ee2823d4499160c18e98c6
Francesco Pieraccini <pieraccini.francesco@gmail.com>
2022-10-21 16:15:50 +0200

    npm install

--- public/js/app.js
+++ public/js/app.js
@@ -49720,8 +49720,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\franc\Documents\boolean-dev\full-stack-dev\php\lezioni\boolpress\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\franc\Documents\boolean-dev\full-stack-dev\php\lezioni\boolpress\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\franc\Documents\boolean-dev\full-stack-dev\php\esercitazioni\laravel-one-to-many\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\franc\Documents\boolean-dev\full-stack-dev\php\esercitazioni\laravel-one-to-many\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })

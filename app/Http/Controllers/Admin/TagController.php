743b85e5bec5f422a398189a10d84c7e188f2b10
Francesco Pieraccini <pieraccini.francesco@gmail.com>
2022-10-24 17:34:32 +0200

    add destroy tag

--- app/Http/Controllers/Admin/TagController.php
+++ app/Http/Controllers/Admin/TagController.php
@@ -100,6 +100,7 @@ class TagController extends Controller
      */
     public function destroy(Tag $tag)
     {
-        //
+        $tag->delete();
+        return redirect()->route('admin.tags.index');
     }
 }
--- resources/views/admin/tags/show.blade.php
+++ resources/views/admin/tags/show.blade.php
@@ -6,12 +6,48 @@
         <div class="row">
             <div class="col-12 d-flex
             align-items-center justify-content-between align-items-center mb-5">
+
                 <a href="{{ route('admin.tags.show', $tag->id) }}" class="badge badge-light text-dark p-3 fs-1 fs-4">
                     {{ $tag->name }}
                 </a>
-                <a href="{{ route('admin.tags.edit', $tag) }}" type="button" class="btn btn-primary btn-sm">
-                    Modifica Tag
-                </a>
+
+                <div>
+                    <a href="{{ route('admin.tags.edit', $tag) }}" type="button" class="btn btn-primary btn-sm">
+                        Modifica Tag
+                    </a>
+                    
+                    <!-- Button trigger modal -->
+                    <button type="button" data-toggle="modal" data-target="#popup" class="btn btn-danger btn-sm">
+                        Elimina
+                    </button>
+                </div>
+                
+                <!-- Modal -->
+                <div class="modal fade" id="popup" tabindex="-1" aria-labelledby="popupLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                        <h5 class="modal-title" id="popupLabel">Elimina tag</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                        </div>
+                        <div class="modal-body">
+                            Eliminare il tag? L'azione è irreversibile...
+                        </div>
+                        <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary btn-sm" data-dismiss="modal">Annulla</button>
+                        <form action="{{ route('admin.tags.destroy', $tag) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+    
+                            <input type="submit" class="btn btn-danger btn-sm" value="Elimina">
+                        </form>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
